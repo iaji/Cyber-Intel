38a338800aa7380ffe3ea4712fff4469fd088e7b
iaji <ajcardar@gmail.com>
2017-06-16 16:54:15 -0400

    Added URL support

--- Tools/InfoSnatch/PatternMatch.py
+++ Tools/InfoSnatch/PatternMatch.py
@@ -44,30 +44,70 @@ def sha1Parse(indicators):
 
     SHA1_regex = r'\b([a-f0-9]{40})\b'
     SHA1s = re.findall(SHA1_regex, indicators, re.IGNORECASE | re.MULTILINE)
+
     if len(SHA1s) == 0:
-        return 'No MD5 hashes'
+        return 'No SHA1 hashes'
     elif len(SHA1s) == 1:
         SHA1s[0] = ('SHA1', SHA1s[0], 'SOURCE?')
         return SHA1s
     else:
         for SHA1 in len(SHA1s):
-            SHA1 = ('SHA1', SHA1, 'SOURCE')
+            SHA1 = ('SHA1', SHA1, 'SOURCE?')
     return SHA1s
 
 def sha256Parse(indicators):
+
+    '''Takes in a text dump and extracts SHA256's then stores them in an Array of tuples.  Formatted (TYPE, CONTENT, SOURCE)'''
+
     SHA256_regex = r'\b[A-Fa-f0-9]{64}\b'
-    SHE256s = re.finditer(SHA256_regex, indicators, re.IGNORECASE | re.MULTILINE)
+    SHA256s = re.findall(SHA256_regex, indicators, re.IGNORECASE | re.MULTILINE)
+
+    if len(SHA256s) == 0:
+        return 'No SHA256 hashes'
+    elif len(SHA256s) == 1:
+        SHA256s[0] = ('SHA256', SHA256s[0], 'SOURCE?')
+        return SHA256s
+    else:
+        for SHA256 in len(SHA256s):
+            SHA256 = ('SHA256', SHA256, 'SOURCE?')
+    return SHA256s
+
 def ipParse(indicators):
+
+    '''Takes in a text dump and extracts IP's then stores them in an Array of tuples.  Formatted (TYPE, CONTENT, SOURCE)'''
+
     ip_regex = r'(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
-    IPs = re.finditer(ip_regex, indicators, re.IGNORECASE | re.MULTILINE)
+    IPs = re.findall(ip_regex, indicators, re.IGNORECASE | re.MULTILINE)
+
+    if len(IPs) == 0:
+        return ['No IPs']
+    elif len(IPs) == 1:
+        IPs[0] = ('IP', IPs[0], 'SOURCE?')
+        return IPs
+    else:
+        for IP in len(IPs):
+            IP = ('IP', IP, 'SOURCE?')
+    return IPs
+
 def urlParse(indicators):
     URL_regex = r'((?:http(?:s)?:\/\/)?)((?:www\.)?[a-zA-Z0-9@:%._\+~#=\-]{2,256}\.[a-z0-9]{2,6}\b)((?:\:\d+)?)((?:[-\w@:%\+.~#&/=]*)?)((?:\?[-\w%\+.~#&=]*)?)'
-    URLs = re.finditer(URL_regex, indicators, re.IGNORECASE | re.MULTILINE)
+    URLs = re.findall(URL_regex, indicators, re.IGNORECASE | re.MULTILINE)
+    print URLs
 
+    if len(URLs) == 0:
+        return ['No URLs']
+
+    elif len(URLs) == 1:
+        URLs[0] = ('URL', URLs[0], 'SOURCE')
+        return URLs
+    else:
+        for URL in len(URLs):
+            URL = ('URL', URL,'SOURCE')
+    return URLs
 
 def main():
     indicators = ('hello\n7d7aaa8c9a36324a2c5e9b0a3440344502f28b90776baa6b8dac7ac88a83aef0\n13cdbd8c31155610b628423dc2720419\n2cb8230281b86fa944d3043ae906016c8b5984d9\nwww.google.com/help\nuc.edu')
 
-    ans = md5Parse(indicators)
+    ans = md5Parse(indicators) + sha1Parse(indicators) + sha256Parse(indicators) + ipParse(indicators)
     print ans
 main()
